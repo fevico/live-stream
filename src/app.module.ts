b1745ac62117b82cb78ae47bf9a1372de6920521
fevico <ajayifavour81@gmail.com>
2026-01-20 23:57:15 +0100

    initial push for live football event

---
+++ src/app.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MatchModule } from './match/match.module';
+import { SupabaseModule } from './supabase/supabase.module';
+
+@Module({
+  imports: [MatchModule, SupabaseModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
